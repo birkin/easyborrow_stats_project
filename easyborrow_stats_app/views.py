d968fb60affb869341f7c6662850885ddbb581ba
birkin <birkin.diana@gmail.com>
2021-02-25 08:48:35 -0500

    adds views for support urls.

--- easyborrow_stats_app/views.py
+++ easyborrow_stats_app/views.py
@@ -1,6 +1,33 @@
+import datetime, json
+
+from django.conf import settings as project_settings
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from easyborrow_stats_app.lib import version_helper
 
 def info(request):
     return HttpResponse("Hello, world. You're at the info page.")
+
+
+# ===========================
+# support urls
+# ===========================
+
+
+def version( request ):
+    """ Returns basic branch and commit data. """
+    rq_now = datetime.datetime.now()
+    commit = version_helper.get_commit()
+    branch = version_helper.get_branch()
+    info_txt = commit.replace( 'commit', branch )
+    context = version_helper.make_context( request, rq_now, info_txt )
+    output = json.dumps( context, sort_keys=True, indent=2 )
+    return HttpResponse( output, content_type='application/json; charset=utf-8' )
+
+
+def error_check( request ):
+    """ For checking that admins receive error-emails. """
+    if project_settings.DEBUG == True:
+        1/0
+    else:
+        return HttpResponseNotFound( '<div>404 / Not Found</div>' )
