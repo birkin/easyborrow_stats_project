c8d52a4bfea75a593b1e365694aff5f84827ca3d
birkin <birkin.diana@gmail.com>
2021-02-26 00:05:37 -0500

    adds two urls to implement.

--- config/urls.py
+++ config/urls.py
@@ -6,10 +6,15 @@ from easyborrow_stats_app import views
 urlpatterns = [
 
     ## primary urls...
-    path('info/', views.info, name='info'),
+
+    path( 'info/', views.info, name='info_url' ),
+    path( 'stats_api/v2/', views.stats, name='stats_api_v2_url' ),
+    path( 'feeds/latest_items/', views.feed, name='feed_url' ),
+
     # path('admin/', admin.site.urls)  ## enabled by default, but disabled here as a reminder that django can be very lightweight
 
     ## support urls...
+
     path( 'version/', views.version, name='version_url' ),
     path( 'error_check/', views.error_check, name='error_check_url' ),
 ]
