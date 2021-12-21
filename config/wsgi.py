b817c9917d2348d08504d33eea2886a2832a076a
Birkin James Diana <birkin.diana@gmail.com>
2021-12-21 15:35:31 -0500

    adds shellvars requirement.

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,16 +1,49 @@
 """
-WSGI config for x_project project.
+WSGI project config.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
+import shellvars
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+PROJECT_DIR_PATH = os.path.dirname( os.path.dirname(os.path.abspath(__file__)) )
+ENV_SETTINGS_FILE_PATH = os.environ['EZB_STATS__ENV_SETTINGS_PATH']  # set in `httpd/passenger.conf`, and `env/bin/activate`
+
+sys.path.append( PROJECT_DIR_PATH )
+
+os.environ[u'DJANGO_SETTINGS_MODULE'] = 'config.settings'  # so django can access its settings
+
+## load up env vars
+var_dct = shellvars.get_vars( ENV_SETTINGS_FILE_PATH )
+for ( key, val ) in var_dct.items():
+    os.environ[key.decode('utf-8')] = val.decode('utf-8')
 
 application = get_wsgi_application()
+
+
+
+
+
+# """
+# WSGI config for x_project project.
+
+# It exposes the WSGI callable as a module-level variable named ``application``.
+
+# For more information on this file, see
+# https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+# """
+
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+# application = get_wsgi_application()
